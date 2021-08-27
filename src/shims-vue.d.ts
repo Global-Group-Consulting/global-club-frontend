082242221e63b808fb1d181a9da3f35bf6dc8dbb
leicaflorian <florian.leica@gmail.com>
2021-08-28 01:40:57 +0200

    updates config and adds libraries

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,16 @@
+import {ComponentCustomProperties} from 'vue'
+import {Store} from 'vuex'
+
 declare module '*.vue' {
-  import { defineComponent } from 'vue'
+  import {defineComponent} from 'vue'
   const component: ReturnType<typeof defineComponent>
+  
   export default component
+}
+
+declare module '@vue/runtime-core' {
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<State>;
+  }
 }
