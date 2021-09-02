1e35020adf8155ae8bd537504b17b918cbb99c8a
leicaflorian <florian.leica@gmail.com>
2021-09-02 23:20:57 +0200

    adds support for SASS

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import './theme/styles.scss';
 
 /* Custom plugins */
 import {authPlugin} from "@/plugins/AuthPlugin"
