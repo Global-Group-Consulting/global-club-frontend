5a2d77e45de60a6e305e87224d922c7303776ee5
leicaflorian <florian.leica@gmail.com>
2021-10-11 16:33:36 +0200

    Updates User page to Profile.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -36,7 +36,7 @@ const privateRoutes = [
   },
   {
     path: '/profile',
-    component: () => import('../views/private/User.vue'),
+    component: () => import('../views/private/Profile.vue'),
     meta: {
       requiresAuth: true
     }
@@ -72,34 +72,6 @@ router.beforeEach(async (to, from, next) => {
   }
   
   next();
-  
-  /*if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (localStorage.getItem('jwt') == null) {
-      next({
-        path: '/login',
-        params: { nextUrl: to.fullPath }
-      })
-    } else {
-      let user = JSON.parse(localStorage.getItem('user'))
-      if (to.matched.some(record => record.meta.is_admin)) {
-        if (user.is_admin == 1) {
-          next()
-        } else {
-          next({ name: 'userboard' })
-        }
-      } else {
-        next()
-      }
-    }
-  } else if (to.matched.some(record => record.meta.guest)) {
-    if (localStorage.getItem('jwt') == null) {
-      next()
-    } else {
-      next({ name: 'userboard' })
-    }
-  } else {
-    next()
-  }*/
 });
 
 export default router;
