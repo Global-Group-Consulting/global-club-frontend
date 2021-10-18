1ac85ee83c363c292fe63adb19df4e2b0945bc81
leicaflorian <florian.leica@gmail.com>
2021-10-18 18:33:40 +0200

    Updates main menu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -8,9 +8,16 @@
 
     <ion-content v-if="isLoggedIn">
       <ion-list>
-        <ion-item>
-          <ion-button @click="$auth.logout()">Logout</ion-button>
+
+        <ion-item button
+                  v-for="(entry, i) of menuEntries" :key="i"
+                  :color="$route.name === entry.route ? 'primary' : ''"
+                  @click="entry.click || $router.push({name: entry.route})">
+          <ion-label>
+            {{ t("mainMenu." + entry.label) }}
+          </ion-label>
         </ion-item>
+
       </ion-list>
     </ion-content>
   </ion-menu>
@@ -18,12 +25,42 @@
 
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 import { useStore } from 'vuex';
 import { key } from '@/store';
+import { AuthPlugin } from '@/plugins/AuthPlugin';
+import { useI18n } from 'vue-i18n';
+import { MessageSchema } from '@/plugins/I18n';
+
+interface MenuEntry {
+  route?: string;
+  click?: () => void;
+  label: string;
+}
 
 const store = useStore(key);
+const { t } = useI18n<{ message: MessageSchema }, 'it'>();
+const auth = inject<AuthPlugin>('auth');
 const isLoggedIn = computed(() => store.getters['auth/isLoggedIn']);
+
+const menuEntries: MenuEntry[] = [
+  {
+    route: 'admin.home',
+    label: 'home',
+  },
+  {
+    route: 'admin.users',
+    label: 'users',
+  },
+  {
+    route: 'admin.products',
+    label: 'products',
+  },
+  {
+    click: () => auth.logout(),
+    label: 'logout',
+  }
+];
 </script>
 
 <style scoped>
