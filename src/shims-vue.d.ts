b4cd11455243bc11e0dcf5533395f6dda6f0acb3
leicaflorian <florian.leica@gmail.com>
2021-10-22 00:41:17 +0200

    Fixes components definition

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,10 +2,9 @@ import {ComponentCustomProperties} from 'vue'
 import {Store} from 'vuex'
 
 declare module '*.vue' {
-  import {defineComponent} from 'vue'
-  const component: ReturnType<typeof defineComponent>
-  
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
 declare module '@vue/runtime-core' {
