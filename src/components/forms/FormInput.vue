ca1e906cf22576470062741a70fb3402d6355348
leicaflorian <florian.leica@gmail.com>
2021-10-22 00:46:10 +0200

    Updates formInput component

--- src/components/forms/FormInput.vue
+++ src/components/forms/FormInput.vue
@@ -2,34 +2,65 @@
   <ion-item class="ion-margin-bottom">
     <ion-label position="floating">{{ label }}</ion-label>
     <component :is="component"
-               :type="componentType"></component>
+               ref="inputComponent"
+               :type="componentType"
+               :value="componentValue"
+               :clearInput="clearInput"
+               :disabled="disabled"
+               :placeholder="placeholder"
+               @ionInput="onInput($event)"
+    ></component>
   </ion-item>
 </template>
 
 <script setup lang="ts">
-  import { computed, defineProps } from 'vue';
+  import { computed, onMounted, ref, watch } from 'vue';
 
-  const props = defineProps<{
+  // @ts-ignore
+  import Cleave from 'cleave.js';
+
+  const props = withDefaults(defineProps<{
     label: string;
-    type: 'text' | 'number' | 'textarea' | "file";
-  }>();
+    modelValue?: string | number;
+    type?: 'date' | 'datetime-local' | 'email' | 'month' | 'number' | 'currency' | 'password' | 'search' | 'tel' | 'text' | 'time' | 'url' | 'week';
+    component?: 'ion-input' | 'ion-textarea';
+    // A hint to the browser for which keyboard to display
+    inputMode?: 'decimal' | 'email' | 'none' | 'numeric' | 'search' | 'tel' | 'text' | 'url' | undefined;
+    // If true, a clear icon will appear in the input when there is a value. Clicking it clears the input.
+    clearInput?: boolean;
+    disabled?: boolean;
+    placeholder?: string;
+  }>(), {
+    type: 'text',
+    component: 'ion-input',
+    clearInput: false,
+    disabled: false
+  });
 
+  // defining emit
+  const emit = defineEmits(['update:modelValue']);
+
+  // Refs
+  const componentValue = ref('');
+  const inputComponent = ref(null); // from the dom refs
+
+  // Computeds
   const component = computed(() => {
-    if (props.type === 'textarea') {
-      return 'ion-textarea';
-    } else {
-      return 'ion-input';
-    }
+    return props.component ?? 'ion-input';
   });
 
   const componentType = computed(() => {
-    if (props.type === 'textarea') {
-      return '';
-    } else {
-      return props.type;
-    }
+    return props.type ?? 'text';
   });
 
+  function onInput (e) {
+    const value = e.target.value;
+    emit('update:modelValue', value);
+  }
+
+  watch(() => props.modelValue, (value) => {
+    componentValue.value = value;
+  }, { immediate: true });
 </script>
 
 <style scoped>
