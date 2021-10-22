48354682932af960b93d0921ca8e2b4eca668f09
leicaflorian <florian.leica@gmail.com>
2021-10-22 17:08:04 +0200

    Adds products fetch on load

--- src/views/private/ProductsList.vue
+++ src/views/private/ProductsList.vue
@@ -1,15 +1,42 @@
 <template>
-  <IonPage> Prodotti </IonPage>
+  <IonPage> Prodotti
+
+    <ul>
+      <li v-for="product of productsList" :key="product._id">
+        <ion-thumbnail slot="start">
+          <ion-img :src="formatImgUrl(product.thumbnail.id)"></ion-img>
+        </ion-thumbnail>
+
+        titolo:
+        {{ product.title }}
+      </li>
+    </ul>
+  </IonPage>
 </template>
 
 <script lang="ts">
-//import { HttpPlugin } from '@/plugins/HttpPlugin';
-import { defineComponent, inject } from "vue";
+  //import { HttpPlugin } from '@/plugins/HttpPlugin';
+  import { defineComponent, inject, onMounted, ref, Ref } from 'vue';
+  import { HttpPlugin } from '@/plugins/HttpPlugin';
+  import { Product } from '@/@types/Product';
+  import { formatImgUrl } from '@/@utilities/images';
 
-export default defineComponent({
-  name: "ProductsList",
-  setup() {
-    //const http: HttpPlugin = inject('http');
-  },
-});
+  export default defineComponent({
+    name: 'ProductsList',
+    setup () {
+      const http = inject<HttpPlugin>('http');
+      const productsList: Ref<Product[]> = ref([]);
+
+      onMounted(async () => {
+        const result = await http?.api.products.readAll() as Product[];
+
+        productsList.value = result;
+      });
+
+      return {
+        productsList,
+        formatImgUrl
+      };
+    },
+  });
 </script>
