8d5e82ce7bee88ee70ef5caf4d0a55b6ec64e340
leicaflorian <florian.leica@gmail.com>
2021-10-27 18:37:23 +0200

    Updates form translations

--- src/locales/it/forms.ts
+++ src/locales/it/forms.ts
@@ -1,10 +1,31 @@
 export const forms = {
+  filePreviewer: {
+    addLabel: "Aggiungi...",
+    removeRemoteFileTooltip: "Cancella il file corrente",
+    removeLocalFileTooltip: "Rimuovi il file corrente"
+  },
   generic: {
-    "filePreviewer": "Aggiungi..."
+    selects: {
+      okText: "Ok",
+      cancelText: "Annulla"
+    }
   },
   productCategories: {
     thumbnail: "Immagine categoria",
     title: "Titolo",
-    description: "Descrizione"
+    description: "Descrizione",
+    btnCreate: "Salva e aggiungi",
+    btnUpdate: "Salva"
+  },
+  products: {
+    thumbnail: "Immagine copertina",
+    images: "Immagini principali",
+    title: "Titolo",
+    description: "Descrizione",
+    price: "Prezzo",
+    tags: "Tag",
+    categories: "Categorie",
+    btnCreate: "Salva e aggiungi",
+    btnUpdate: "Salva"
   }
 }
