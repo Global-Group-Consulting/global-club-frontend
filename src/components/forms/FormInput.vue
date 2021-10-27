004d84dfb7be94dc3095ce58f6b022286d9e02ee
leicaflorian <florian.leica@gmail.com>
2021-10-27 18:36:08 +0200

    Extends fileInput component by adding ion-select

--- src/components/forms/FormInput.vue
+++ src/components/forms/FormInput.vue
@@ -8,28 +8,41 @@
                :clearInput="clearInput"
                :disabled="disabled"
                :placeholder="placeholder"
+               :multiple="multiple"
+               :okText="selectBtnOk || $t('forms.generic.selects.okText')"
+               :cancelText="selectBtnCancel || $t('forms.generic.selects.cancelText')"
                @ionInput="onInput($event)"
-    ></component>
+               @ionChange="onChange($event)"
+    >
+      <template v-if="component === 'ion-select'">
+        <ion-select-option v-for="option of options"
+                           :key="option.value"
+                           :value="option.value">
+          {{ option.text }}
+        </ion-select-option>
+      </template>
+    </component>
   </ion-item>
 </template>
 
 <script setup lang="ts">
-  import { computed, onMounted, ref, watch } from 'vue';
-
-  // @ts-ignore
-  import Cleave from 'cleave.js';
+  import { computed, inject, Ref, ref, watch } from 'vue';
 
   const props = withDefaults(defineProps<{
     label: string;
-    modelValue?: string | number;
+    modelValue?: string | number | string[];
     type?: 'date' | 'datetime-local' | 'email' | 'month' | 'number' | 'currency' | 'password' | 'search' | 'tel' | 'text' | 'time' | 'url' | 'week';
-    component?: 'ion-input' | 'ion-textarea';
+    component?: 'ion-input' | 'ion-textarea' | 'ion-select';
     // A hint to the browser for which keyboard to display
     inputMode?: 'decimal' | 'email' | 'none' | 'numeric' | 'search' | 'tel' | 'text' | 'url' | undefined;
     // If true, a clear icons will appear in the input when there is a value. Clicking it clears the input.
     clearInput?: boolean;
     disabled?: boolean;
     placeholder?: string;
+    options?: { text: string; value: string }[];
+    multiple?: boolean;
+    selectBtnOk?: string;
+    selectBtnCancel?: string;
   }>(), {
     type: 'text',
     component: 'ion-input',
@@ -41,7 +54,7 @@
   const emit = defineEmits(['update:modelValue']);
 
   // Refs
-  const componentValue = ref('');
+  const componentValue: Ref<string | string[] | number | undefined> = ref();
   const inputComponent = ref(null); // from the dom refs
 
   // Computeds
@@ -55,6 +68,16 @@
 
   function onInput (e) {
     const value = e.target.value;
+    emit('update:modelValue', value);
+  }
+
+  function onChange (e) {
+    const value = e.target.value;
+
+    if (props.component !== "ion-select") {
+      return;
+    }
+
     emit('update:modelValue', value);
   }
 
