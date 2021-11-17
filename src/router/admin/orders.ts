9ed470e849d3495335ec16c157b482d4dbe06c7a
leicaflorian <florian.leica@gmail.com>
2021-11-17 18:06:25 +0100

    Add Orders route

--- src/router/admin/orders.ts
+++ src/router/admin/orders.ts
@@ -1,8 +1,18 @@
+const prefix = "/admin/orders";
+
 export default [
   {
-    path: "/admin/orders",
+    path: prefix,
     component: () => import("@/views/admin/orders/Home.vue"),
     name: "admin.orders",
+    meta: {
+      isAdmin: true,
+    },
+  },
+  {
+    path: prefix + "/:id",
+    component: () => import("@/views/admin/orders/Details.vue"),
+    name: "admin.orders.details",
     meta: {
       isAdmin: true,
     },
