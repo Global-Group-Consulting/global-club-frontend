479e9331e0d259bc6913ea81ceed7dd54517aa38
leicaflorian <florian.leica@gmail.com>
2021-11-18 18:59:02 +0100

    Adds basic Chat component

---
+++ src/components/chats/Chat.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="chat-component">
+    <div v-if="communication.messages && communication.messages.length > 0"
+         class="chat-container">
+      <ChatMessage v-for="message in communication.messages" :key="message._id"
+                   :data="message"></ChatMessage>
+    </div>
+
+    <p v-else>
+      Nessun messaggio disponibile
+    </p>
+
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  import { Communication } from '@/@types/Communication';
+  import ChatMessage from '@/components/chats/ChatMessage.vue';
+
+  export default defineComponent({
+    name: "Chat",
+    components: { ChatMessage },
+    props: {
+      communication: {
+        type: Object as PropType<Communication>,
+        default: () => ({})
+      }
+    }
+  });
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/chats/ChatMessage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="chat-message">
+    <div v-html="data.content"></div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  import { Message } from '@/@types/Communication';
+
+  export default defineComponent({
+    name: "ChatMessage",
+    props: {
+      data: {
+        type: Object as PropType<Message>,
+        required: true
+      }
+    }
+  });
+</script>
+
+<style scoped>
+
+</style>
