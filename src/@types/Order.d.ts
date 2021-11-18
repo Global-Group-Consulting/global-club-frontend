4095c62769e157a4e19469f43345ebcc353431cc
leicaflorian <florian.leica@gmail.com>
2021-11-18 18:18:26 +0100

    Adds missing types

--- src/@types/Order.d.ts
+++ src/@types/Order.d.ts
@@ -1,11 +1,12 @@
 import { User, UserBasic } from '@/@types/User';
 import { OrderStatusEnum } from '@/@enums/order.status.enum';
+import { Communication } from '@/@types/Communication';
 
 export interface Order {
   user: UserBasic;
   products: OrderProduct[];
   amount: number;
-  communication: any; //Communication;
+  communication: Communication;
   status: OrderStatusEnum;
   
   _id: string;
@@ -23,4 +24,9 @@ export interface OrderProduct {
   the one user saw when adding the product to the cart
    */
   price: number;
+}
+
+export interface ReadOrderStatusesDto {
+  _id: OrderStatusEnum;
+  count: number;
 }
