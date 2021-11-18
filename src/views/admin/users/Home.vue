7ec5bef58249867b6bd76f92225069903b642166
leicaflorian <florian.leica@gmail.com>
2021-11-18 18:16:43 +0100

    Uses Tabs component inside users view

--- src/views/admin/users/Home.vue
+++ src/views/admin/users/Home.vue
@@ -14,16 +14,7 @@
                 </SimpleToolbar>-->
 
 
-        <ion-segment v-model="activeTab">
-          <ion-segment-button :value="group._id"
-                              v-for="group in groupsList" :key="group._id">
-            <ion-label>
-              {{ $t("enums.UserRoleEnum." + UserRoleEnum[group._id]) }}
-              ({{ group.count }})
-            </ion-label>
-          </ion-segment-button>
-        </ion-segment>
-
+        <Tabs :tabs-list="groupsList" v-model="activeTab"></Tabs>
 
         <ion-list>
           <ion-item v-for="user of usersList" :key="user._id">
@@ -54,23 +45,35 @@
   import { computed, ComputedRef, defineComponent, inject, Ref, ref, watch } from 'vue';
   import { onIonViewWillEnter } from '@ionic/vue';
   import TopToolbar from '@/components/toolbars/TopToolbar.vue';
-  import { ReadUserGroupsDto, User } from '@/@types/User';
   import { HttpPlugin } from '@/plugins/HttpPlugin';
   import { UserRoleEnum } from '@/@enums/user.role.enum';
-  // import { useI18n } from 'vue-i18n';
   import { PaginatedResult } from '@/@types/Pagination';
   import PaginationBar from '@/components/PaginationBar.vue';
+  import { TabEntry } from '@/@types/TabEntry';
+  import { useI18n } from 'vue-i18n';
+  import Tabs from '@/components/Tabs.vue';
+  import { User } from '@/@types/User';
 
   export default defineComponent({
     name: "UsersPage",
-    components: { PaginationBar, TopToolbar },
+    components: { Tabs, PaginationBar, TopToolbar },
     setup () {
       const http = inject<HttpPlugin>("http") as HttpPlugin
-      // const { t } = useI18n()
+      const { t } = useI18n()
 
       const paginatedUsersData: Ref<PaginatedResult<User[]>> = ref({} as any);
       const activeTab: Ref<UserRoleEnum> = ref(UserRoleEnum.CLIENTE)
-      const groupsList: Ref<ReadUserGroupsDto[]> = ref([])
+      const groupsList: Ref<TabEntry[]> = ref([
+        {
+          id: UserRoleEnum.CLIENTE,
+          text: t("enums.UserRoleEnum." + UserRoleEnum[UserRoleEnum.CLIENTE]),
+          count: 0
+        }, {
+          id: UserRoleEnum.AGENTE,
+          text: t("enums.UserRoleEnum." + UserRoleEnum[UserRoleEnum.AGENTE]),
+          count: 0
+        }
+      ])
 
       const usersList: ComputedRef<User[]> = computed(() => {
         return paginatedUsersData.value.data
@@ -100,8 +103,15 @@
           http.api.users.readGroups(),
           fetchUsers()
         ])
-        groupsList.value = result[0] ?? []
-        // await fetchUsers()
+
+        groupsList.value.forEach(entry => {
+          const fetchedGroups = result[0];
+          const group = fetchedGroups?.find(el => el._id === entry.id);
+
+          if (group) {
+            entry.count = group.count
+          }
+        })
       });
 
       return {
