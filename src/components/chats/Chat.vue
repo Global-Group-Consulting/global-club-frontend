16454ac947d5437e77d02cc9cc7f1a093d78fb08
leicaflorian <florian.leica@gmail.com>
2021-11-22 14:40:49 +0100

    Updates chat component by adding response btn and formatting incoming data

--- src/components/chats/Chat.vue
+++ src/components/chats/Chat.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="chat-component">
-    <div v-if="communication.messages && communication.messages.length > 0"
-         class="chat-container">
-      <ChatMessage v-for="message in communication.messages" :key="message._id"
-                   :data="message"></ChatMessage>
+    <div v-if="communication.messages && communication.messages.length > 0">
+      <div class="chat-container">
+        <ChatMessage v-for="message in communication.messages" :key="message._id"
+                     :data="message"></ChatMessage>
+      </div>
+
+      <div class="mt-3 mx-auto w-lg-50 w-75">
+        <Btn expand="block" @click="onAnswerClick">Rispondi</Btn>
+      </div>
     </div>
 
     <p v-else>
@@ -14,17 +19,46 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
+  import { defineComponent, inject, PropType } from 'vue';
   import { Communication } from '@/@types/Communication';
   import ChatMessage from '@/components/chats/ChatMessage.vue';
+  import ChatAnswerModal from '@/components/modals/ChatAnswerModal.vue';
+  import Btn from '@/components/Btn.vue';
+  import { modalController } from '@ionic/vue';
+  import { HttpPlugin } from '@/plugins/HttpPlugin';
 
   export default defineComponent({
     name: "Chat",
-    components: { ChatMessage },
+    components: { Btn, ChatMessage },
     props: {
       communication: {
         type: Object as PropType<Communication>,
         default: () => ({})
+      }
+    },
+    setup () {
+      const http = inject<HttpPlugin>('http') as HttpPlugin;
+
+      async function onAnswerClick () {
+        const modal = await modalController
+            .create({
+              component: ChatAnswerModal,
+              componentProps: {
+                title: 'Nuovo risposta al messaggio'
+              },
+            })
+
+        await modal.present();
+
+        const result = await modal.onWillDismiss();
+
+        if (result.role === "ok") {
+          // await http.api
+        }
+      }
+
+      return {
+        onAnswerClick
       }
     }
   });
