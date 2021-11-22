a2eba0ffb28bba0b8112bcbb9e489ea78c0ab936
Gerta <gerta.habibi@chweb.it>
2021-11-22 17:02:05 +0100

    Modified css/added nav bar

--- src/views/private/OrdersHistory.vue
+++ src/views/private/OrdersHistory.vue
@@ -1,7 +1,12 @@
 <template>
   <IonPage>
+    <div class="header-nav"> 
+      <TopToolbar include-back>I miei ordini</TopToolbar>
+        </div>
     <IonContent class="ion-padding">
- <ion-grid class="ion-no-padding">
+
+
+ <ion-grid class="ion-no-padding history-content">
         <ion-row>
           <ion-col>
             <div class="ion-text-start">Ordini in corso</div>
@@ -12,15 +17,15 @@
       <ion-grid class="stato-banner">
         <ion-row>
           <ion-col size="2">
-            <ion-img src="/assets/workout-pic.png"></ion-img>
+            <img src="/assets/workout-pic.png">
           </ion-col>
           <ion-col size="8">
             <span>Ordine del 20/09/21</span><br>
             Stato: <span class="lavorazione">In lavorazione</span>
             <ion-progress-bar class="progress" color="primary" value="0.8"></ion-progress-bar>
           </ion-col>
-          <ion-col size="2">
-            <ion-icon src="./assets/icons/plus.svg"></ion-icon>
+          <ion-col size="2" class="ion-text-end">
+            <div class="icon-position"><ion-icon src="./assets/icons/plus.svg"></ion-icon></div>
           </ion-col>
 
         </ion-row>
@@ -37,15 +42,15 @@
       <ion-grid class="stato-banner">
         <ion-row>
           <ion-col size="2">
-            <ion-img src="/assets/workout-pic.png"></ion-img>
+            <img src="/assets/workout-pic.png">
           </ion-col>
           <ion-col size="8">
             <span> Ordine del 20/09/21</span><br>
             Stato: <span class="completato">Completato</span>
             <ion-progress-bar class="progress" color="primary" value="1"></ion-progress-bar>
           </ion-col>
-          <ion-col size="2">
-            <ion-icon src="./assets/icons/plus.svg"></ion-icon>
+          <ion-col size="2" class="ion-text-end">
+            <div class="icon-position"><ion-icon src="./assets/icons/plus.svg"></ion-icon></div>
           </ion-col>
 
         </ion-row>
@@ -54,15 +59,15 @@
       <ion-grid class="stato-banner">
         <ion-row>
           <ion-col size="2">
-            <ion-img src="/assets/workout-pic.png"></ion-img>
+            <img src="/assets/workout-pic.png">
           </ion-col>
           <ion-col size="8">
             <span> Ordine del 20/09/21</span><br>
              Stato: <span class="annullato">Annullato</span>
             <ion-progress-bar class="progress" color="primary" value="1"></ion-progress-bar>
           </ion-col>
-          <ion-col size="2">
-            <ion-icon src="./assets/icons/plus.svg"></ion-icon>
+          <ion-col size="2" class="ion-text-end">
+           <div class="icon-position"><ion-icon src="./assets/icons/plus.svg"></ion-icon></div>
           </ion-col>
 
         </ion-row>
@@ -71,15 +76,15 @@
        <ion-grid class="stato-banner">
         <ion-row>
           <ion-col size="2">
-            <ion-img src="/assets/workout-pic.png"></ion-img>
+           <img src="/assets/workout-pic.png">
           </ion-col>
           <ion-col size="8">
             <span> Ordine del 20/09/21</span><br>
             Stato: <span class="completato">Completato</span>
             <ion-progress-bar class="progress" color="primary" value="1"></ion-progress-bar>
           </ion-col>
-          <ion-col size="2">
-            <ion-icon src="./assets/icons/plus.svg"></ion-icon>
+          <ion-col size="2" class="ion-text-end">
+            <div class="icon-position"><ion-icon src="./assets/icons/plus.svg"></ion-icon></div>
           </ion-col>
 
         </ion-row>
@@ -104,9 +109,10 @@ export default defineComponent({
 <style>
 .stato-banner {
 background-color: rgb(30, 30, 30);
-border-radius: 30px;
+border-radius: 15px;
 text-align: left;
 font-size: 13px;
+padding:10px;
 }
 
 
@@ -126,4 +132,19 @@ margin-top: 6px;
 .lavorazione {
   color:#c9a866;
 }
+
+.header-nav{
+color: white;
+padding-left: 30px;
+padding-right: 75px;
+}
+
+.history-content {
+  margin-top: 40px;
+}
+
+.icon-position{
+  padding-top: 20px;
+}
+
 </style>
