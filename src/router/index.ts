07e0e9b3f8aaaa3fe1aefff203c2722456fa8785
leicaflorian <florian.leica@gmail.com>
2021-11-30 16:17:43 +0100

    Activates pages permissions

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,28 +21,27 @@ router.beforeEach(async (to, from, next) => {
   // Due to the fact that this could be accessed
   // before the AuthPlugin gets completed, we await that this first gets initiated
   await AuthPlugin.isInitiated;
-  /*const loggedIn = store.getters['auth/isLoggedIn'];
+  const userIsAdmin = store.getters['auth/isAdmin'];
+  const loggedIn = store.getters['auth/isLoggedIn'];
   
   const isPrivateRoute = to.matched.some((record) => record.meta.isPrivate);
   const isAdminRoute = to.matched.some((record) => record.meta.isAdmin);
   const isGuestRoute = to.matched.some((record) => record.meta.guest);
   
-  const userIsAdmin = store.getters['auth/isAdmin'];*/
   //const userIsPrivate = store.getters['auth/isNormal'];
-
-  /*if ((isPrivateRoute || isAdminRoute) && !loggedIn) {
+  
+  if ((isPrivateRoute || isAdminRoute) && !loggedIn) {
     return next({
-      path: 'guest.home',
+      name: 'public.home',
     });
   } else if (loggedIn) { // if user is logged in
     let newPage;
     
-    // If guest route
-    if (isGuestRoute) {
+    if (isGuestRoute) { // Public route
       newPage = 'private.home';
-    } else if ((isAdminRoute && !isPrivateRoute) && !userIsAdmin) {
+    } else if ((isAdminRoute && !isPrivateRoute) && !userIsAdmin) { // Private route
       newPage = 'private.home';
-    } else if ((isPrivateRoute && !isAdminRoute) && userIsAdmin) {
+    } else if ((isPrivateRoute && !isAdminRoute) && userIsAdmin) { // Admin route
       newPage = 'admin.home';
     }
     
@@ -51,7 +50,7 @@ router.beforeEach(async (to, from, next) => {
         name: newPage,
       });
     }
-  }*/
+  }
 
   next();
 });
