6fe95c937cd1ed8db5bb60ab5f3ffc4517b2836e
leicaflorian <florian.leica@gmail.com>
2021-12-01 18:57:57 +0100

    Update UserContractForm component

---
+++ src/@enums/pack.enum.ts
@@ -0,0 +1,5 @@
+export enum PackEnum {
+  BASIC = "basic",
+  FAST = "fast",
+  PREMIUM = "premium",
+}
--- src/components/forms/UserContractForm.vue
+++ src/components/forms/UserContractForm.vue
@@ -1,14 +1,30 @@
 <template>
   <ion-grid class="p-0">
+
+    must add vee validate form component
+    https://vee-validate.logaretm.com/v4/guide/components/handling-forms
+
+    <h3>Contratto Club</h3>
+    <ion-row class="mb-5">
+      <ion-col v-bind="cols">
+        <FormInput label="N° tessera Club" :readonly="readonly" v-model="formData.clubCardNumber"></FormInput>
+      </ion-col>
+      <ion-col v-bind="cols">
+        <FormInput label="Pacchetto attivo" :readonly="readonly" v-model="formData.clubPack"></FormInput>
+      </ion-col>
+    </ion-row>
+
+    <h3>Contratto Global</h3>
     <ion-row>
-      <ion-col>
-        <FormInput label="Label dell'input" :readonly="readonly" modelValue="valore del campo"></FormInput>
+      <ion-col v-bind="cols">
+        <FormInput label="N° contratto Global" :readonly="readonly" v-model="formData.contractNumber"></FormInput>
       </ion-col>
-      <ion-col>
-        <FormInput label="Label dell'input" :readonly="readonly" modelValue="valore del campo"></FormInput>
+      <ion-col v-bind="cols">
+        <FormInput label="Data Stipula contratto" :readonly="readonly" v-model="formData.contractDate"></FormInput>
       </ion-col>
-      <ion-col>
-        <FormInput label="Label dell'input" :readonly="readonly" modelValue="valore del campo"></FormInput>
+      <ion-col v-bind="cols">
+        <FormInput label="Percentuale assegnata" :readonly="readonly"
+                   :model-value="user.contractPercentage"></FormInput>
       </ion-col>
     </ion-row>
 
@@ -16,7 +32,7 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
+  import { computed, defineComponent, PropType, Ref, ref, watch } from 'vue';
   import FormInput from '@/components/forms/FormInput.vue';
   import { User } from '@/@types/User';
 
@@ -29,6 +45,25 @@
         type: Object as PropType<User>
       },
       readonly: Boolean
+    },
+    setup (props) {
+      const cols = computed(() => {
+        return {
+          "sizeLg": 4,
+          "sizeMd": 6,
+          "size": 12,
+        }
+      })
+
+      const formData: Ref<User> = ref({} as User);
+
+      watch(() => props.user, value => {
+        Object.assign(formData.value, value)
+      }, { immediate: true })
+
+      return {
+        cols, formData
+      }
     }
   });
 </script>
