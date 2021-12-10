d0860543824ae515207689fc6fa26af2386026d1
leicaflorian <florian.leica@gmail.com>
2021-12-10 23:09:06 +0100

    Renamed AccountPopover.vue to MenuDropdownPopover.vue

---
+++ src/components/popovers/MenuDropdownPopover.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-content>
+    <ion-list>
+      <MainMenuList :entries-list="data" @click="onItemClick"
+                    :title="title"></MainMenuList>
+    </ion-list>
+  </ion-content>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import menuEntries from '@/composables/menuEntries';
+  import MainMenuList from '@/components/lists/MainMenuList.vue';
+
+  export default defineComponent({
+    name: "MenuDropdownPopover",
+    components: { MainMenuList },
+    props: {
+      data: Array,
+      title: {
+        type: String,
+      }
+    },
+    setup () {
+      const { onItemClick } = menuEntries();
+
+      return {
+        onItemClick,
+      }
+    }
+  });
+</script>
+
