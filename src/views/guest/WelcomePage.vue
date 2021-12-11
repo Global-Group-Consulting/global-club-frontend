41c46b635c1b1edc7dae289125210dca7abafee6
leicaflorian <florian.leica@gmail.com>
2021-12-11 14:56:36 +0100

    Updates welcome page

--- src/views/guest/WelcomePage.vue
+++ src/views/guest/WelcomePage.vue
@@ -1,50 +1,33 @@
 
 <template>
   <IonPage>
-    <IonContent class="content-with-fixed-btn ion-padding">
+    <IonHeader>
       <div class="logo-container"></div>
+    </IonHeader>
+
+    <IonContent>
       <ion-grid fixed>
         <ion-row class="ion-justify-content-center">
           <ion-col sizeLg="6" sizeMd="7" sizeSm="8">
-            <div class="mt-10vh ion-text-center">
-              <p class="text-large">
-                We are so glad to welcome you to Global Club App!<br />
+            <div class="mt-5 ion-text-center">
+              <h3>
+                We are so glad to welcome you to Global Club App!<br/>
                 Please login to your account to start tracking all your movement
                 and enjoy it!
-              </p>
+              </h3>
             </div>
           </ion-col>
         </ion-row>
       </ion-grid>
-      <!--
-      <ion-grid fixed>
-        <ion-row class="ion-justify-content-end">
-          <ion-col sizeLg="6" sizeMd="7" sizeSm="8">
-            &lt;!&ndash;            &ndash;&gt;
-          </ion-col>
-        </ion-row>
-      </ion-grid>-->
+
     </IonContent>
-    <ion-footer>
-      <ion-grid fixed class="grid-login">
-        <ion-row class="ion-justify-content-center">
-          <ion-col sizeLg="6" sizeMd="7" sizeSm="8" class="p-0">
-            <ClubButton
-              size="large"
-              expanded
-              @click="$router.push('/login')"
-            >
-              Iniziamo
-            </ClubButton>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
-    </ion-footer>
+
+    <TheFooterButton label="Iniziamo" @click="$router.push({name: 'public.login'})"/>
   </IonPage>
 </template>
 
 <script lang="ts" setup>
-import ClubButton from '@/components/ClubButton.vue';
+  import TheFooterButton from '@/components/TheFooterButton.vue';
 </script>
 
 <style scoped>
