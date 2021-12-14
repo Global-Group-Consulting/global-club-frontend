70c1049a96f609ad7547c6c116487059ba5c9af1
leicaflorian <florian.leica@gmail.com>
2021-12-14 18:34:34 +0100

    Adds BriteValue.vue component

---
+++ src/components/BriteValue.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="d-inline-flex ion-align-items-center">
+    <BriteIcon class="me-2" v-if="!symbolAfter"/>
+
+    <span>
+      {{ formatBrites(value) }}
+    </span>
+
+    <BriteIcon class="ms-2" v-if="symbolAfter"/>
+  </span>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import BriteIcon from '@/components/BriteIcon.vue';
+  import { formatBrites } from '@/@utilities/currency';
+
+  export default defineComponent({
+    name: "BriteValue",
+    components: { BriteIcon },
+    props: {
+      value: Number,
+      symbolAfter: Boolean
+    },
+    setup () {
+      return {
+        formatBrites
+      }
+    }
+  });
+</script>
+
+<style scoped>
+
+</style>
