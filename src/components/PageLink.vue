9631db3db2ffdbc7f566594c754b3b3ef0920376
leicaflorian <florian.leica@gmail.com>
2022-05-09 21:33:42 +0200

    Updated pathResolve function by addmin query params

--- src/components/PageLink.vue
+++ src/components/PageLink.vue
@@ -18,81 +18,89 @@
 </template>
 
 <script lang="ts">
-  import { RouteLocationRaw, useRouter } from 'vue-router';
-  import { computed, defineComponent, PropType } from 'vue';
-  import ClubButton from '@/components/ClubButton.vue';
+import { RouteLocationRaw, useRouter } from 'vue-router'
+import { computed, defineComponent, PropType } from 'vue'
+import ClubButton from '@/components/ClubButton.vue'
 
-  export interface IonButtonInterface {
-    disabled?: boolean;
-    strong?: boolean;
-    fill?: 'default' | 'clear' | 'outline' | 'solid';
-    mode?: import('@ionic/core').Mode;
-    type?: 'button' | 'reset' | 'submit';
-    color?: string;
-    size?: 'small' | 'default' | 'large';
-    modelValue?: string | boolean;
-    buttonType?: string;
-    expand?: 'full' | 'block';
-    routerDirection?: import('@ionic/core').RouterDirection;
-    routerAnimation?: import('@ionic/core').AnimationBuilder;
-    download?: string;
-    href?: string;
-    rel?: string;
-    shape?: 'round';
-    target?: string;
-  }
+export interface IonButtonInterface {
+  disabled?: boolean;
+  strong?: boolean;
+  fill?: 'default' | 'clear' | 'outline' | 'solid';
+  mode?: import('@ionic/core').Mode;
+  type?: 'button' | 'reset' | 'submit';
+  color?: string;
+  size?: 'small' | 'default' | 'large';
+  modelValue?: string | boolean;
+  buttonType?: string;
+  expand?: 'full' | 'block';
+  routerDirection?: import('@ionic/core').RouterDirection;
+  routerAnimation?: import('@ionic/core').AnimationBuilder;
+  download?: string;
+  href?: string;
+  rel?: string;
+  shape?: 'round';
+  target?: string;
+}
 
-  export default defineComponent({
-    components: { ClubButton },
-    props: {
-      to: {
-        type: Object as PropType<RouteLocationRaw>
-      },
-      btnProps: Object as PropType<IonButtonInterface>,
-      onlyContainer: Boolean,
-      class: String
+export default defineComponent({
+  components: { ClubButton },
+  props: {
+    to: {
+      type: Object as PropType<RouteLocationRaw>
     },
-    setup (props, { attrs }) {
-      const router = useRouter();
+    btnProps: Object as PropType<IonButtonInterface>,
+    onlyContainer: Boolean,
+    class: String
+  },
+  setup (props, { attrs }) {
+    const router = useRouter()
 
-      const resolvedPath = computed(() => {
-        if (!props.to) {
-          return ""
-        }
-
-        return router.resolve(props.to).path;
-      });
-
-      const ionBtnProps = computed(() => {
-        return Object.assign({
-          version: "link"
-        }, props.btnProps, attrs);
-      });
-
-      function onClick (navigate, e) {
-        const keys = ["metaKey", "ctrlKey"].reduce((acc, key) => {
-          if (e[key]) {
-            acc[key] = e[key]
-          }
-
-          return acc;
-        }, {})
-
-        if (props.btnProps?.target !== "_blank" && Object.keys(keys).length === 0) {
-          e.preventDefault()
-
-          navigate()
-        }
-
+    const resolvedPath = computed(() => {
+      if (!props.to) {
+        return ''
       }
 
-      return {
-        resolvedPath,
-        ionBtnProps,
-        onClick
+      let resolvedPath = router.resolve(props.to).path
+
+      if (props.to['query']) {
+        const query = props.to['query']
+
+        resolvedPath += '?' + Object.keys(query).map(key => key + '=' + query[key]).join('&')
       }
+
+      return resolvedPath
+    })
+
+    const ionBtnProps = computed(() => {
+      return Object.assign({
+        version: 'link'
+      }, props.btnProps, attrs)
+    })
+
+    function onClick (navigate, e) {
+      const keys = ['metaKey', 'ctrlKey'].reduce((acc, key) => {
+        if (e[key]) {
+          acc[key] = e[key]
+        }
+
+        return acc
+      }, {})
+
+      if (props.btnProps?.target !== '_blank' && Object.keys(keys).length === 0) {
+        e.preventDefault()
+
+        navigate()
+      }
+
     }
-  })
+
+    return {
+      resolvedPath,
+      ionBtnProps,
+      onClick
+    }
+  }
+})
 
 </script>
 
