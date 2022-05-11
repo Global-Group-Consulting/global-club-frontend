dc0c117b0319de08ef25812cead0804e5aa0b6ff
leicaflorian <florian.leica@gmail.com>
2022-05-11 16:21:39 +0200

    Adds pack expiration date

--- src/views/private/dashboard/Index.vue
+++ src/views/private/dashboard/Index.vue
@@ -3,36 +3,27 @@
     <IonContent>
       <LogoToolbar/>
 
+      <BannerPremium></BannerPremium>
+
       <ion-grid fixed>
 
-        <div class="mb-5">
-          <ion-text color="medium"><h6 class="ion-text-left mb-0">
-            Bentornat{{ authUser.gender === 'f' ? "a" : 'o' }},</h6></ion-text>
+        <ion-row class="mb-4">
+          <ion-col size="12" sizeMd="6">
+            <ion-text color="medium" class="ion-text-left">
+              <h6 class="my-0">Bentornat{{ authUser.gender === 'f' ? 'a' : 'o' }},</h6>
+              <h4 class="my-0">{{ $store.getters['auth/fullName'] }}</h4>
+            </ion-text>
+          </ion-col>
 
-          <div class="d-flex ion-justify-content-between ion-align-items-center">
-            <div class="ion-text-left">
-              <h4 class="mt-2  mb-0"> {{ $store.getters["auth/fullName"] }} </h4>
-            </div>
-            <div class="ion-text-right">
-              <h4 class="mt-2 mb-0" style="color: var(--ion-color-primary)" v-if="userIsPremium">
+          <ion-col size="12" sizeMd="6" class="ion-text-start ion-text-md-end">
+            <ion-text color="primary">
+              <h4 class="my-0" v-if="userIsPremium">
                 Cliente Premium
               </h4>
-
-              <ClubButton v-if="!userIsPremium && !authUser.clubPackChangeOrder" class="m-0" @click="changePack"
-                          :disabled="authUser.clubPackChangeOrder">Passa a
-                premium
-              </ClubButton>
-
-              <PageLink class="mt-2 mb-0"
-                        v-if="authUser.clubPackChangeOrder"
-                        title="Vai alla richiesta"
-                        :to="{name: 'private.orders.details', params: {id: authUser.clubPackChangeOrder}}"
-                        :btn-props="{version:'link', icon: true, iconName:'time-circle'}">
-                Cambio pack in corso...
-              </PageLink>
-            </div>
-          </div>
-        </div>
+              <h6 class="my-0" v-if="clubPackExpiration">Scadenza: {{ clubPackExpiration }}</h6>
+            </ion-text>
+          </ion-col>
+        </ion-row>
 
         <div class="mb-5">
           <SearchBar @update:filters="onSearchUpdate"></SearchBar>
@@ -52,62 +43,82 @@
 </template>
 
 <script lang="ts">
-import {computed, ComputedRef, defineComponent, inject, ref} from "vue";
-import {useStore} from "vuex";
-import {storeKey} from "@/store";
-import {User} from "@/@types/User";
-import {OrderStatusEnum} from '@/@enums/order.status.enum';
-import SearchBar from '@/components/SearchBar.vue';
-import PrivateOrdersList from '@/components/lists/orders/PrivateOrdersList.vue';
-import LogoToolbar from '@/components/toolbars/LogoToolbar.vue';
-import UserStatistics from '@/components/UserStatistics.vue';
-import {useRouter} from 'vue-router';
-import Filters from '@/composables/filters';
-import {PackEnum} from '@/@enums/pack.enum';
-import ClubButton from '@/components/ClubButton.vue';
-import {AlertsPlugin} from "@/plugins/Alerts";
-import {HttpPlugin} from "@/plugins/HttpPlugin";
-import PageLink from "@/components/PageLink.vue";
+import { computed, ComputedRef, defineComponent, inject, ref } from 'vue'
+import { useStore } from 'vuex'
+import { storeKey } from '@/store'
+import { User } from '@/@types/User'
+import { OrderStatusEnum } from '@/@enums/order.status.enum'
+import SearchBar from '@/components/SearchBar.vue'
+import PrivateOrdersList from '@/components/lists/orders/PrivateOrdersList.vue'
+import LogoToolbar from '@/components/toolbars/LogoToolbar.vue'
+import UserStatistics from '@/components/UserStatistics.vue'
+import { useRouter } from 'vue-router'
+import Filters from '@/composables/filters'
+import { PackEnum } from '@/@enums/pack.enum'
+import ClubButton from '@/components/ClubButton.vue'
+import { AlertsPlugin } from '@/plugins/Alerts'
+import { HttpPlugin } from '@/plugins/HttpPlugin'
+import PageLink from '@/components/PageLink.vue'
+import BannerPremium from '@/components/BannerPremium.vue'
+import { formatLocaleDate } from '@/@utilities/dates'
 
 export default defineComponent({
-  name: "Dashboard",
-  components: {PageLink, ClubButton, UserStatistics, LogoToolbar, PrivateOrdersList, SearchBar},
-  setup() {
-    const store = useStore(storeKey);
-    const router = useRouter();
-    const alerts = inject("alerts") as AlertsPlugin;
-    const http = inject("http") as HttpPlugin;
+  name: 'Dashboard',
+  components: { BannerPremium, UserStatistics, LogoToolbar, PrivateOrdersList, SearchBar },
+  setup () {
+    const store = useStore(storeKey)
+    const router = useRouter()
+    const alerts = inject('alerts') as AlertsPlugin
+    const http = inject('http') as HttpPlugin
     const authUser: ComputedRef<User> = computed(
-        () => store.getters["auth/user"]
-    );
-    const firstName = ref<string>(authUser?.value?.firstName);
-    const pendingStatuses = [OrderStatusEnum.PENDING, OrderStatusEnum.IN_PROGRESS];
+        () => store.getters['auth/user']
+    )
+    const firstName = ref<string>(authUser?.value?.firstName)
+    const pendingStatuses = [OrderStatusEnum.PENDING, OrderStatusEnum.IN_PROGRESS]
     const filtersComposable = Filters()
 
     const userIsPremium = computed(() => {
-      return authUser.value.clubPack === PackEnum.PREMIUM;
+      return authUser.value.clubPack === PackEnum.PREMIUM
     })
 
-    function onSearchUpdate(filters) {
-      router.push({name: "private.products", query: filtersComposable.prepareFilters(filters)})
+    /**
+     * Check if the user has some Premium pack change order and return the expiration date
+     */
+    const clubPackExpiration = computed(() => {
+      const history = authUser.value.clubPackHistory ?? []
+      const lastChange = history[history.length - 1] ?? null
+
+      if (!lastChange) {
+        return null
+      }
+
+      if (lastChange.pack === PackEnum.PREMIUM && userIsPremium.value) {
+        return formatLocaleDate(new Date(lastChange.endsAt), true)
+      }
+
+      return null
+    })
+
+    function onSearchUpdate (filters) {
+      router.push({ name: 'private.products', query: filtersComposable.prepareFilters(filters) })
     }
 
-    async function changePack() {
+    async function changePack () {
       const answer = await alerts.ask({
-        header: "Passa a premium!",
-        message: "Gentile utente, state per richiedere il passaggio al Pack Premium. Se desiderate continuare, vi preghiamo di cliccare su 'OK'."
-      });
+        header: 'Passa a premium!',
+        message: 'Gentile utente, state per richiedere il passaggio al Pack Premium. Se desiderate continuare, vi preghiamo di cliccare su \'OK\'.'
+      })
 
       if (!answer.resp) {
         return
       }
 
-      const result = await http.api.users.updatePack(store.getters["auth/user"]._id);
+      const result = await http.api.users.updatePack(store.getters['auth/user']._id)
 
       if (result) {
-        await store.dispatch("auth/updateUser", {
+        await store.dispatch('auth/updateUser', {
           clubPackChangeOrder: result.clubPackChangeOrder
-        });
+        })
       }
     }
 
@@ -115,11 +126,12 @@ export default defineComponent({
       authUser,
       firstName,
       pendingStatuses, userIsPremium,
+      clubPackExpiration,
       onSearchUpdate,
       changePack
-    };
-  },
-});
+    }
+  }
+})
 </script>
 
 
