e1b65240eeb761ad24a8483a2e625b5037d6f2da
= <=>
2022-07-09 11:39:13 +0200

    Adds top-badge slot

--- src/components/tabs/TabsItems.vue
+++ src/components/tabs/TabsItems.vue
@@ -7,40 +7,44 @@
     <ion-segment-button v-for="tab in tabsList" :key="tab.id"
                         :value="tab.id" class="tabs-item"
                         layout="icon-bottom">
+
       <ion-label>
         {{ tab.text }} <span v-if="tab.count">({{ tab.count }})</span>
         <ion-badge v-if="tab.unreadCount"
                    color="danger" class="unread" style="width: 10px; height: 10px">&nbsp;
         </ion-badge>
       </ion-label>
+
+      <slot name="top-badge" v-bind:tab="tab"></slot>
+
     </ion-segment-button>
   </ion-segment>
 </template>
 
 <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
-  import { TabEntry } from '@/@types/TabEntry';
+import { defineComponent, PropType } from 'vue'
+import { TabEntry } from '@/@types/TabEntry'
 
-  export default defineComponent({
-    name: 'TabsItems',
-    props: {
-      tabsList: {
-        required: true,
-        type: Array as PropType<TabEntry[]>
-      },
-      modelValue: [String, Number]
+export default defineComponent({
+  name: 'TabsItems',
+  props: {
+    tabsList: {
+      required: true,
+      type: Array as PropType<TabEntry[]>
     },
-    setup (props, { emit }) {
-      function onSelectionChange (ev) {
-        const value = ev.target.value
+    modelValue: [String, Number]
+  },
+  setup (props, { emit }) {
+    function onSelectionChange (ev) {
+      const value = ev.target.value
 
-        emit('update:modelValue', value)
-      }
-
-      return {
-        onSelectionChange
-      }
+      emit('update:modelValue', value)
     }
-  })
+
+    return {
+      onSelectionChange
+    }
+  }
+})
 
 </script>
